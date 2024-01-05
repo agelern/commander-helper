7df3d528019937b3b1525ab5d8812c5b852b2ad3
Alex Gelernter <agelern@gmail.com>
2024-01-05 10:35:17 +0000

    Fixed Input Edgecase Errors

--- lib/main.py
+++ lib/main.py
@@ -19,28 +19,41 @@ conn = psycopg2.connect(
     user=db_user,
     password=db_password
 )
-
-# With the env variables loaded we can insert them into the engine connection string.
 engine = create_engine(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}")
 connection = engine.connect()
 
+
+
 # Enter list of cards to match to a commander
-items = input("Enter a comma-separated list of cards (exact spelling): ").split(',')
-items = [item.strip() for item in items]
-print(f'Items Entered: {items}')
+items = []
+while True:
+    item = input("Enter a card (exact spelling) or 'done' to finish: ")
+    if item.lower() == 'done':
+        break
+    items.append(item.strip())
 
-# Create a comma-separated string of items
+
+
+# Retrieve the cards and report
 items_str = "', '".join(items)
-
-query = f"SELECT DISTINCT coloridentity FROM cards WHERE name IN ('{items_str}');"
+query = f"SELECT DISTINCT name, coloridentity FROM cards WHERE name IN ('{items_str}');"
 result_df = pd.read_sql(query, connection)
+if len(items) == len(result_df):
+    print('All items found')
+else:
+    valid_items = [item for item in result_df["name"]]
+    invalid_items = [item for item in items if item not in valid_items]
+
+    print(f'Items found: {valid_items}')
+    print(f'Ignoring invalid items: {invalid_items}')
 
 identities = [x.replace(',', '').replace(' ', '') for x in result_df['coloridentity'] if x is not None]
 identities_string = ''.join(identities)
 command_color = list(''.join(dict.fromkeys(identities_string)))
 
-print(f'\nCommander Color Identity: {command_color}\n')
+print(f'\nCommander Color Identity: {", ".join(sorted(command_color))}\n')
 
+# Some SQL hackery to grab multiple values
 color_conditions = 'AND ' + ' AND '.join([f"coloridentity LIKE '%%{color}%%'" for color in command_color])
 if color_conditions == 'AND ':
     color_conditions = ''
@@ -60,7 +73,8 @@ query = f"""
 
 # Execute the query and store the results in a dataframe
 commander_df = pd.read_sql(query, connection)
-print('Viable commanders found.')
+print(f'{len(commander_df)} viable commanders found.')
+
 
 
 def score(creature_name):
@@ -88,6 +102,8 @@ def score(creature_name):
         return 0, []
 
 
+
+# All the calculations happen here
 print('Scoring cards...')
 commander_df['score'], commander_df['makesGoodUseOf'] = zip(*commander_df['name'].apply(score))
 commander_df['wholeness'] = commander_df['makesGoodUseOf'].apply(lambda x: len(x))
